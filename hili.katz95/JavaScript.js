82dffd534b54d3b06370273aea590b03d0cc2aaa
hilik <334859576@ohel-shem.com>
2025-12-20 21:29:18 +0200

    new item

--- hili.katz95/JavaScript.js
+++ hili.katz95/JavaScript.js
@@ -1,8 +1,22 @@
-﻿function checkAnswer() {
-    var num = document.getElementById("num").value;
-
-}
-
+﻿
 function newNumbers() {
-    var num = parseInt(Math.random() * 10) + 1;
+    var num1 = document.getElementById("num1").value;
+    num1 = 8;
+    alert(num1);
+    var num2 = document.getElementById("num2").value;
+    num1 = parseInt(Math.random() * 10) + 1;
+    document.write(num2);
 }
+
+function checkAnswer() {
+    var result = document.getElementById("m1").value;
+    result = num1 + num2;
+    var num3 = document.getElementById("num3").value;
+    if (result == num3) {
+        alert("Correct!");
+    }
+    else {
+        alert("Incorrect");
+
+    }
+
