4aea5e9a5ea032db1d50ce42c72589751be55218
hilik <334859576@ohel-shem.com>
2025-12-30 20:43:58 +0200

    new item

--- hili.katz95/Ex22.js
+++ hili.katz95/Ex22.js
@@ -1,7 +1,15 @@
-﻿var div = document.getElementById("aElement");
+﻿//targil1
+var div = document.getElementById("aElement");
 var aEl = document.createElement("a");
 aEl.setAttribute("href", "http://www.google.com");
 aEl.innerHTML = "GOOGLE";
 div.appendChild(aEl);
 //targil2
 var div2 = document.getElementById("imgElement");
+var imgEl = document.createElement("img");
+imgEl.setAttribute("src", "nof.jpg", "https://www.example.com/image.jpg");
+imgEl.setAttribute("height", "200");
+imgEl.setAttribute("width", "200");
+div2.appendChild(imgEl);
+//targil3
+var div3 = document.getElementById("pElement");
