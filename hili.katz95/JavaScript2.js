46ea6747ef8b3bf15eb6d889c5c6e07f1516d549
334859576-sys <334859576@ohel-shem.com>
2026-01-12 21:07:07 +0200

    Rename check function to checkAnswer

--- hili.katz95/JavaScript2.js
+++ hili.katz95/JavaScript2.js
@@ -1,4 +1,4 @@
-﻿let num1 = 0;
+let num1 = 0;
 let num2 = 0;
 
 let a = 0;
@@ -33,7 +33,7 @@ function newNumbers() {
 
 }
 
-function check() {
+function checkAnswer() {
     let num3 = Number(document.getElementById("num3").value);
     if (num3 == num1 + num2) {
         document.getElementById("m1").value = "Correct";
